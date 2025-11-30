6e424074530d5733a1e309b5a4137db6f0e4e74e
KhushbooChaudhari10 <khushboochaudhari224@gmail.com>
2025-11-30 09:57:49 +0530

    added AI-Backend/app/__pycache__ AI-Backend/__pycache__

--- AI_CORE/AI_Storyteller/run_storyteller.py
+++ AI_CORE/AI_Storyteller/run_storyteller.py
@@ -1,7 +1,7 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-drawing_file = os.path.join(BASE_DIR, "sample_drawings", "hand_draw.jpeg")
+drawing_file = os.path.join(BASE_DIR, "sample_drawing", "hand_draw.jpeg")
 # audio_file = os.path.join(BASE_DIR, "audio", "audio5.wav")
 
 from agent.storyteller_agent import run_agent
